9c91e13e2670159ece46dd9cbbd0cd23a738d494
Max-Joseph <e1862695@cmaisonneuve.qc.ca>
2023-03-13 12:04:21 -0400

    Avancement globale sur la scène de crédit et gestion du système de chrono

--- Assets/Script/ScriptParScene/ScriptMenus/ScriptCredit/GestionCredit.cs
+++ Assets/Script/ScriptParScene/ScriptMenus/ScriptCredit/GestionCredit.cs
@@ -1,23 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 
 public class GestionCredit : MonoBehaviour
 {
+    float time;
+    public float TimerInterval = 5;
+    float tick;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Awake()
+    {
+        time = (int)Time.time;
+        tick = TimerInterval;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Mouse0))
+        print(time);
+
+        time = (int)Time.time;
+
+        if(time == tick)
         {
+            tick = time + TimerInterval;
+            Debug.Log("Timer");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0) || time == 10)
+        {
+            GetComponent<Animator>().SetTrigger("FinVideo");
             Invoke("RetourIntro", 1);
         }
+
     }
 
     void RetourIntro()
