31aafc7d510e44e953447f0e503ceaa249a00669
Max-Joseph <e1862695@cmaisonneuve.qc.ca>
2023-03-27 11:56:01 -0400

    Ajout du UI dans le niv 1, ajout d'un écran niv 5 dans choix de niveau et barre de vie

--- Assets/Script/ScriptMulti/ScriptMultiNiv/GestionChrono.cs
+++ Assets/Script/ScriptMulti/ScriptMultiNiv/GestionChrono.cs
@@ -6,15 +6,6 @@ using UnityEngine.UI;
 
 public class GestionChrono : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
