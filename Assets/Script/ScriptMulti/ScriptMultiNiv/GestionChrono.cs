ff19b2ad5e7d52ff8bdeea90f2919b29912a5fb0
Max-Joseph <e1862695@cmaisonneuve.qc.ca>
2023-04-03 12:30:43 -0400

    Conception complète d'un système de chrono avec la possibilité de mettre en pause, gestion complète du système d'option et  système complète de la barre de vie

--- Assets/Script/ScriptMulti/ScriptMultiNiv/GestionChrono.cs
+++ Assets/Script/ScriptMulti/ScriptMultiNiv/GestionChrono.cs
@@ -6,11 +6,52 @@ using UnityEngine.UI;
 
 public class GestionChrono : MonoBehaviour
 {
+    public TextMeshProUGUI texteChrono;
+    public float chrono = 0;
+    public bool chronoEnCours = true;
 
-
-    public void PauseChrono()
+    void Start()
     {
+       chronoEnCours = true;
 
     }
+
+    void Update()
+    {
+        if (chronoEnCours)
+        {
+            if( chrono >= 0)
+            {
+                chrono += Time.deltaTime;
+                DisplayTime(chrono);
+            }
+        }
+    }
+    void DisplayTime(float tempsASeparer)
+    {
+        tempsASeparer += 1;
+
+        string minutes = ((int)tempsASeparer / 60).ToString();
+        string secondes = (tempsASeparer % 60).ToString("f2");
+
+        texteChrono.text = minutes + ":" + secondes;
+
+
+        //float minutes = Mathf.FloorToInt(tempsASeparer/60);
+        //float secondes = Mathf.FloorToInt(tempsASeparer%60);
+        //texteChrono.text = string.Format("{00:00} : {01:00}",minutes, secondes);
+    }
+
+    public void ChronoPause()
+    {
+        chronoEnCours = false;
+    }
+
+    public void Chronoreprise()
+    {
+        chronoEnCours = true;
+    }
+
+
 }
 
