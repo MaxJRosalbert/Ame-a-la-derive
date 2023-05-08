a414e323318185661ba4ce8112185df08611ddb1
Max-J Rosalbert <rosalmjo@gmail.com>
2023-05-08 07:22:47 -0400

    lundi 8 mai mise a jour UI et dialogue, il manque les bugs et l'audio

--- Assets/Script/ScriptMulti/ScriptMultiNiv/GestionInteraction.cs
+++ Assets/Script/ScriptMulti/ScriptMultiNiv/GestionInteraction.cs
@@ -4,26 +4,88 @@ using UnityEngine;
 
 public class GestionInteraction : MonoBehaviour
 {
+    bool bulleDialogue1 = false;
+    bool bulleDialogue2 = false;
+    bool bulleDialogue3 = false;
 
     public GameObject dialogue1;
+    public GameObject dialogue2;
+    public GameObject dialogue3;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            Invoke("BoiteDialogueNiv",0f);
+            if (bulleDialogue1 == false)
+            {
+                Invoke("OBoiteDialoguePS", 0f);
+                bulleDialogue1 = true;
+            }
+            else
+            {
+                Invoke("FBoiteDialoguePS", 0f);
+                bulleDialogue1 = false;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            Invoke("BoiteDialogueNiv", 0f);
+            if (bulleDialogue2 == false)
+            {
+                Invoke("OBoiteDialogueP2", 0f);
+                bulleDialogue2 = true;
+            }
+            else
+            {
+                Invoke("FBoiteDialogueP2", 0f);
+                bulleDialogue2 = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (bulleDialogue3 == false)
+            {
+                Invoke("OBoiteDialoguePNJ", 0f);
+                bulleDialogue3 = true;
+            }
+            else
+            {
+                Invoke("FBoiteDialoguePNJ", 0f);
+                bulleDialogue3 = false;
+            }
         }
 
     }
 
-    public void BoiteDialogueNiv()
+    public void OBoiteDialoguePS()
     {
         dialogue1.SetActive(true);
+    }
+
+    public void OBoiteDialogueP2()
+    {
+        dialogue2.SetActive(true);
+    }
+
+    public void OBoiteDialoguePNJ()
+    {
+        dialogue3.SetActive(true);
+    }
+
+    public void FBoiteDialoguePS()
+    {
+        dialogue1.SetActive(false);
+    }
+
+    public void FBoiteDialogueP2()
+    {
+        dialogue2.SetActive(false);
+    }
+
+    public void FBoiteDialoguePNJ()
+    {
+        dialogue3.SetActive(false);
     }
 }
---
+++ Assets/Script/ScriptParScene/ScriptMenus/ScriptCinematique/GestionCinematique1.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GestionCinematique1 : MonoBehaviour
+{
+    float compteur = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Chrono());
+        compteur += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(compteur);
+
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GetComponent<Animator>().SetTrigger("FinVideo");
+            Invoke("Niv2", 1);
+        }
+
+    }
+
+    IEnumerator Chrono()
+    {
+
+
+        while (compteur > 0)
+        {
+            compteur--;
+            yield return new WaitForSeconds(1f);
+
+        }
+
+        if (compteur == 0)
+        {
+            Invoke("Niv2", 1);
+        }
+
+    }
+
+    void Niv2()
+    {
+        SceneManager.LoadScene("niveau-2");
+        compteur = 5;
+        print(compteur);
+    }
+}
